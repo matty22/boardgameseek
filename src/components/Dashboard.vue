7637677d9fca19123ef61ed01168cb930c567bc2
Matt <matt.leonard.22@gmail.com>
2021-01-14 15:04:33 -0800

    Rename files, basic styles, css reset

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside id="leftCol">
+    <div id="title">
+      <p>BoardGameSeek</p>
+    </div>
+    <ul>
+      <li>Dashboard</li>
+      <li>Recommender</li>
+      <li>About</li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  setup() {
+
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+}
+
+#leftCol {
+  width: 200px;
+  background-color: #212121;
+  color: #ffffff;
+  min-height: 100vh;
+ 
+}
+
+#title {
+ 
+}
+
+
+
+</style>
