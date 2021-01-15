235d7fada01aa2b9b948182ed57a301b2ba6c372
Matt Leonard <matt.leonard.22@protonmail.com>
2021-01-15 13:41:16 -0800

    Revert "Fleshes out basic UI"

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,22 +1,14 @@
 <template>
-  <div id="body">
-    <aside>
-      <div id="title">
-        <h3>BoardGameSeek</h3>
-      </div>
-      <ul>
-        <li>DASHBOARD</li>
-        <li>RECOMMENDER</li>
-        <li>ABOUT</li>
-      </ul>
-    </aside>
-    <main>
-      <section id="infoBar"></section>
-      <section id="topRow"></section>
-      <section id="middleRow"></section>
-      <section id="bottomRow"></section>
-    </main>
-  </div>
+  <aside id="leftCol">
+    <div id="title">
+      <p>BoardGameSeek</p>
+    </div>
+    <ul>
+      <li>Dashboard</li>
+      <li>Recommender</li>
+      <li>About</li>
+    </ul>
+  </aside>
 </template>
 
 <script>
@@ -32,75 +24,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-/* Set up grid for entire dashboard */
-#body {
-  display: grid;
-  grid-template-columns: 200px auto;
+ul {
+  list-style-type: none;
 }
 
-/* Brown nav window */
-aside {
+#leftCol {
+  width: 200px;
   background-color: #212121;
   color: #ffffff;
   min-height: 100vh;
-  padding: 20px 0px;
+ 
 }
 
-/* Brown nav window title - BoardGameSeek */
-aside > #title {
-  margin: 0 15px;
-  text-align: center;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #ffffff;
+#title {
+ 
 }
 
-/* Navigation UL */
-aside > ul {
-  list-style-type: none;
-  padding: 40px 20px;
-  font-size: 14px;
-}
-
-aside > ul > li {
-  padding-bottom: 30px;
-}
-
-/* Main window styles */
-
-main {
-  display: grid;
-  grid-row-gap: 20px;
-  grid-template-rows: 35px 150px 300px 200px;
-  background-color: #f4f3ef;
-  padding: 0px 20px;  
-}
-
-#infoBar {
-  background-color: #f4f8ef;
-  border-bottom: 1px solid #dadada;
-}
-
-#topRow {
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: auto auto auto auto;
-  grid-column-gap: 20px;
-}
-
-#middleRow {
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: auto auto;
-  grid-column-gap: 20px;
-}
-
-#bottomRow {
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: auto auto;
-  grid-column-gap: 20px;
-}
 
 
 </style>
